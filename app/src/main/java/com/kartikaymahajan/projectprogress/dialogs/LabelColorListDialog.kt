c65cc7c2f4e5414bc4c4dac3c2fe7023a94b3df4
Kartikay <mahajankartikay401@gmail.com>
2021-05-26 19:57:29 +0530

    initial commit

---
+++ app/src/main/java/com/kartikaymahajan/projectprogress/dialogs/LabelColorListDialog.kt
@@ -0,0 +1,51 @@
+package com.kartikaymahajan.projectprogress.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.icu.text.CaseMap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kartikaymahajan.projectprogress.R
+import com.kartikaymahajan.projectprogress.adapters.LabelColorListItemsAdapter
+import kotlinx.android.synthetic.main.activity_sign_in.view.*
+import kotlinx.android.synthetic.main.dialog_list.view.*
+
+abstract class LabelColorListDialog(
+    context: Context,
+    private var list: ArrayList<String>,
+    private val title: String = "",
+    private val mSelectedColor: String = ""
+) : Dialog(context) {
+
+    private var adapter: LabelColorListItemsAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState ?: Bundle())
+
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_list, null)
+
+        setContentView(view)
+        setCanceledOnTouchOutside(true)
+        setCancelable(true)
+        setUpRecyclerView(view)
+    }
+
+    private fun setUpRecyclerView(view: View) {
+        view.tvTitle.text = title
+        view.rvList.layoutManager = LinearLayoutManager(context)
+        adapter = LabelColorListItemsAdapter(context, list, mSelectedColor)
+        view.rvList.adapter = adapter
+
+        adapter!!.onItemClickListener = object : LabelColorListItemsAdapter.OnItemClickListener {
+
+            override fun onClick(position: Int, color: String) {
+                dismiss()
+                onItemSelected(color)
+            }
+        }
+    }
+
+    protected abstract fun onItemSelected(color: String)
+}
